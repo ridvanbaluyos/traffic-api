f760832c7017a498069403339d79c26e26c20633
ridvanbaluyos <ridvan@baluyos.net>
2015-02-26 05:39:04 +0800

    [Added] - Checking if parameter is valid or not.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ use Ridvanbaluyos\Mmda\MMDA as MMDA;
 
 $app = new \Slim\Slim(
     array(
-        'debug' => true,
+        'debug' => false,
     )
 );
 
 $app->get('/', function () {
-    echo "Hello world!";
+    echo '{"status":"SUCCESS", "code":"200", "response":"nothing here! :)"}';
 });
 
 // V1 route group
@@ -20,17 +20,30 @@ $app->group('/v1', function () use ($app) {
         $response = false;
 
         if (!is_null($highway) && !is_null($segment) && !is_null($direction)) {
-            $response = $traffic[$highway][$segment][$direction];
+            if (isset($traffic[$highway][$segment][$direction])) {
+                $response = $traffic[$highway][$segment][$direction];
+            }
         } else if (!is_null($highway) && !is_null($segment)) {
-            $response = $traffic[$highway][$segment];
+            if (isset($traffic[$highway][$segment])) {
+                $response = $traffic[$highway][$segment];
+            }
         } else if (!is_null($highway)) {
-            $response = $traffic[$highway];
+            if (isset($traffic[$highway])) {
+                $response = $traffic[$highway];
+            }
         } else {
-            $response = $traffic;
+            if (isset($traffic)) {
+                $response = $traffic;
+            }
         }
 
-        $response = json_encode($response);
-        echo '{"status":"SUCCESS", "code":"200", "response" : ' . $response . '}';
+        // Response
+        if (!$response) {
+            echo '{"status":"NOT_ACCEPTABLE", "code":"406"}';
+        } else {
+            $response = json_encode($response);
+            echo '{"status":"SUCCESS", "code":"200", "response" : ' . $response . '}';
+        }
     });
 });
 $app->run();
