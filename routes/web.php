c80039018d43487bd43bef6cadf034edfa4ff67f
Ridvan Baluyos <ridvan@baluyos.net>
2018-04-06 17:19:36 +0800

    upgrade lumen from 5.4 to 5.6

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all of the routes for an routerlication.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->version();
+$router->get('/', function () use ($router) {
+    return $router->version();
 });
 
-$app->get('/test', function () use ($app) {
+$router->get('/test', function () use ($router) {
     echo 'test';
     exit;
 });
 
 // Version 1
-$app->group(['prefix' => 'v1'], function () use ($app) {
-    $app->get('feed', 'v1\TrafficController@getTrafficFeed');
-    $app->get('highways', 'v1\TrafficController@getHighways');
-    $app->get('segments', 'v1\TrafficController@getSegments');
+$router->group(['prefix' => 'v1'], function () use ($router) {
+    $router->get('feed', 'v1\TrafficController@getTrafficFeed');
+    $router->get('highways', 'v1\TrafficController@getHighways');
+    $router->get('segments', 'v1\TrafficController@getSegments');
 });
